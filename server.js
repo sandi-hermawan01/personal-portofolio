f4df33cd64238ec4e52acef076cdde27d4f285f5
sandi-hermawan01 <sandihermawanf01@gmail.com>
2023-12-07 07:44:04 +0700

    blog feature maintenace

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post("/upload", (req, res) => {
 });
 
 app.get("/admin", (req, res) => {
-  res.sendFile(path.join(initial_path, "dashboard.html"));
+  res.sendFile(path.join(basic_path, "redirect-page.html"));
 });
 
 app.get("/:blog", (req, res) => {
