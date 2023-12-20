4090e47223114b0557d0afb3235a2f3f0abde55a
sandi-hermawan01 <sandihermawanf01@gmail.com>
2023-12-20 23:12:15 +0700

    update background

--- public/app.js
+++ public/app.js
@@ -14,40 +14,40 @@ let githubBtn = document.querySelector("#github-btn");
 let progressTrack = [...document.querySelectorAll(".progress-track")];
 
 //hacker effect
-// var canvas = document.getElementById("canvas");
-// var context = canvas.getContext("2d");
-// var W = window.innerWidth;
-// var H = window.innerHeight;
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var W = window.innerWidth;
+var H = window.innerHeight;
 
-// canvas.width = W;
-// canvas.height = H;
+canvas.width = W;
+canvas.height = H;
 
-// var fontSize = 16;
-// var columns = Math.floor(W / fontSize);
-// var drops = [];
-// for (var i = 0; i < columns; i++) {
-//   drops.push(0);
-// }
+var fontSize = 18;
+var columns = Math.floor(W / fontSize);
+var drops = [];
+for (var i = 0; i < columns; i++) {
+  drops.push(0);
+}
 
-// var str = "Zsans Product - Sandi Portofolio";
-// function draw() {
-//   context.fillStyle = "rgba(0,0,0,0.05)";
-//   context.fillRect(0, 0, W, H);
-//   context.fontSize = "700 " + fontSize + "px";
-//   context.fillStyle = "#00cc33";
-//   for (var i = 0; i < columns; i++) {
-//     var index = Math.floor(Math.random() * str.length);
-//     var x = i * fontSize;
-//     var y = drops[i] * fontSize;
-//     context.fillText(str[index], x, y);
-//     if (y >= canvas.height && Math.random() > 0.99) {
-//       drops[i] = 0;
-//     }
-//     drops[i]++;
-//   }
-// }
-// draw();
-// setInterval(draw, 35);
+var str = "Zsans Product - Sandi Portofolio";
+function draw() {
+  context.fillStyle = "rgba(0,0,0,0.05)";
+  context.fillRect(0, 0, W, H);
+  context.fontSize = "700 " + fontSize + "px";
+  context.fillStyle = "#a1a1a1";
+  for (var i = 0; i < columns; i++) {
+    var index = Math.floor(Math.random() * str.length);
+    var x = i * fontSize;
+    var y = drops[i] * fontSize;
+    context.fillText(str[index], x, y);
+    if (y >= canvas.height && Math.random() > 0.99) {
+      drops[i] = 0;
+    }
+    drops[i]++;
+  }
+}
+draw();
+setInterval(draw, 35);
 
 //particle affect
 // particlesJS("particles", {
@@ -263,34 +263,34 @@ function sendMail() {
 }
 
 //lamp
-function updateTimeAndDate() {
-  const now = new Date();
-  let hours = now.getHours();
-  const minutes = now.getMinutes().toString().padStart(2, "0");
-  let amPm = hours >= 12 ? "PM" : "AM";
-  if (hours > 12) {
-    hours -= 12;
-  } else if (hours === 0) {
-    hours = 12;
-  }
-  let timeStr = hours.toString().padStart(2, "0") + minutes;
-  if (timeStr.startsWith("0")) {
-    timeStr = " " + timeStr.slice(1);
-  }
-  let month = (now.getMonth() + 1).toString().padStart(2, "0");
-  let day = now.getDate().toString().padStart(2, "0");
-  const year = now.getFullYear().toString().slice(-2);
-  if (month.startsWith("0")) {
-    month = " " + month.slice(1);
-  }
-  if (day.startsWith("0")) {
-    day = " " + day.slice(1);
-  }
-  const displayStr = timeStr + amPm + month + day + year;
-  for (let i = 0; i < 12; i++) {
-    document.getElementById("char" + i + "1").textContent = displayStr[i];
-    document.getElementById("char" + i + "2").textContent = displayStr[i];
-  }
-}
-updateTimeAndDate();
-setInterval(updateTimeAndDate, 60000);
+// function updateTimeAndDate() {
+//   const now = new Date();
+//   let hours = now.getHours();
+//   const minutes = now.getMinutes().toString().padStart(2, "0");
+//   let amPm = hours >= 12 ? "PM" : "AM";
+//   if (hours > 12) {
+//     hours -= 12;
+//   } else if (hours === 0) {
+//     hours = 12;
+//   }
+//   let timeStr = hours.toString().padStart(2, "0") + minutes;
+//   if (timeStr.startsWith("0")) {
+//     timeStr = " " + timeStr.slice(1);
+//   }
+//   let month = (now.getMonth() + 1).toString().padStart(2, "0");
+//   let day = now.getDate().toString().padStart(2, "0");
+//   const year = now.getFullYear().toString().slice(-2);
+//   if (month.startsWith("0")) {
+//     month = " " + month.slice(1);
+//   }
+//   if (day.startsWith("0")) {
+//     day = " " + day.slice(1);
+//   }
+//   const displayStr = timeStr + amPm + month + day + year;
+//   for (let i = 0; i < 12; i++) {
+//     document.getElementById("char" + i + "1").textContent = displayStr[i];
+//     document.getElementById("char" + i + "2").textContent = displayStr[i];
+//   }
+// }
+// updateTimeAndDate();
+// setInterval(updateTimeAndDate, 60000);
