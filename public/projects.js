adcfea1e81bb4bdc35edf9ab89f88b1fb46623ca
sandi-hermawan01 <sandihermawanf01@gmail.com>
2024-02-24 20:14:22 +0700

    update project link

--- public/projects.js
+++ public/projects.js
@@ -3,7 +3,7 @@ let projects = [
     name: "Portofolio v2",
     about: "Personal portofolio second vesion.",
     tags: "#react, #fullstack, #html, #css",
-    live: "",
+    live: "https://zsans-product.online",
     github: "",
     image: "./assets/img/portofoliov2.jpg",
     languages: {
